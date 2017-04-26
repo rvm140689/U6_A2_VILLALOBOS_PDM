aa547eeee20e2674c42ad41332b96d990b660044
Raul <rvm_140689@comunidad.unam.mx>
2017-04-25 20:20:03 -0500

    Initial commit

---
+++ app/src/test/java/pdm/villalobos/a1/u6/u6_a2_villalobos_pdm/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package pdm.villalobos.a1.u6.u6_a2_villalobos_pdm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
